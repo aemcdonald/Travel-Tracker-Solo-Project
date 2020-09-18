e9b3ecb987254a496027f2f69ff55b224f1aeed8
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-18 17:36:19 -0600

    Create test for instance of Traveler

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -3,12 +3,19 @@ import travelers from './travelers-data.js';
 import Traveler from '../src/Traveler.js';
 
 describe('Traveler', () => {
-  let traveler;
+  let traveler1, traveler2, traveler3;
 
   beforeEach(() => {
+    traveler1 = new Traveler(travelers[0]);
+    traveler2 = new Traveler(travelers[1]);
+    traveler3 = new Traveler(travelers[2]);
   });
 
   it('should be a function', () => {
     expect(Traveler).to.be.a('function');
   });
+
+  it('should be an instance Traveler', () => {
+    expect(traveler1).to.be.an.instanceOf(Traveler);
+  })
 });
