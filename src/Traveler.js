62f7f5154776b2985430264444756ad251d6e2b7
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-18 16:55:38 -0600

    Add initial Traveler class structure

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,7 +1,10 @@
 class Traveler {
-  constructor() {
-
+  constructor(id, name, travelerType) {
+    this.id = id;
+    this.name = name;
+    this.travelerType = travelerType;
   }
+  //method that shows past trips taken
 }
 
 module.exports = Traveler;
