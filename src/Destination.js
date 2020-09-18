f04c0c90979895a168611438065f82f1b709459e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-18 17:00:02 -0600

    Create initial Destination class structure

---
+++ src/Destination.js
@@ -0,0 +1,9 @@
+class Destination {
+  constructor(id, lodgingCostPerDay, flightCostPerPerson) {
+    this.id = id;
+    this.lodgingCostPerDay = lodgingCostPerDay;
+    this.flightCostPerPerson = flightCostPerPerson;
+  }
+}
+
+module.exports = Destination;
