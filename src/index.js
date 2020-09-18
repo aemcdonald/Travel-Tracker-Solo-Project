d71294da41e8dcb18aaf54036ed38efc3272aeac
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-18 17:00:47 -0600

    Add commits to fix Webpack Issue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import getSingleUser from './apiCalls.js'; //import not working for some reason?
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
@@ -8,3 +9,23 @@ import './css/base.scss';
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+//const user = getSingleUser()
+
+// const getSingleUser = () => {
+//   return fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/1`)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//   })
+// }
+
+let user;
+function grabUserData() {
+  Promise.all([getSingleUser()])
+  .then(value => {
+    let user = value[0]; //value 0 is referring to the first Promise returned in the Promise.all
+    console.log(user)
+  })
+}
+
+grabUserData()
