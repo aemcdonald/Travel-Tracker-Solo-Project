a628166b89134f8209b9bbe7d6d281fafc7855af
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 10:39:50 -0600

    Create fetchAllDestinationsData method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,10 +8,17 @@ let apiCalls = {
     .then(data => data.travelers)
     .catch(error => console.log(error))
   },
+  //fetch single user?? I don't think I need to...
   fetchAllTripsData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
     .then(response => response.json())
     .then(data => data.trips)
+    .catch(error => console.log(error))
+  },
+  fetchAllDestinationsData() {
+    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+    .then(response => response.json())
+    .then (data => data.destinations)
     .catch(error => console.log(error))
   }
 }
