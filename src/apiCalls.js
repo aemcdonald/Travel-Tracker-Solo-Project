18b663d944fdb970a9fe43c0050db38e8acc5428
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 09:44:40 -0600

    Create apiCalls method to fetchAllUsersData

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,27 @@
 //https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/1
 //where<id> will be a number of a traveler’s id
 
-const getSingleUser = () => {
-  return fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/1`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-  })
+let apiCalls = {
+  fetchAllUsersData() {
+    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .catch(error => console.log(error))
+  } //need comma here!
 }
 
-// export default {
-//   getSingleUser
+
+// const getSingleUser = () => {
+//   return fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/1`)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//   })
 // }
-if (typeof module !== 'undefined') {
-  module.exports = getSingleUser;
-}
+
+
+// if (typeof module !== 'undefined') {
+//   module.exports = getSingleUser;
+// }
+
+export default apiCalls
