d409e080547d7dcbc4fdb7a51a04cedb8e252a16
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 14:22:55 -0600

    Create test for instance of trip

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -1,14 +1,20 @@
 import { expect } from 'chai';
 import trips from './trips-data.js';
 import Trip from '../src/Trip.js';
+import Traveler from '../src/Trip.js'
 
 describe('Trip', () => {
   let trip;
 
   beforeEach(() => {
+    trip = new Trip(trips[0])
   });
 
   it('should be a function', () => {
     expect(Trip).to.be.a('function');
+  });
+
+  it('should be an instance of a trip', () => {
+    expect(trip).to.be.an.instanceof(Trip);
   });
 });
