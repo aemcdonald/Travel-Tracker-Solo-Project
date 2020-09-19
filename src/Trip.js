d9289849a1a84d1743375a62b1a65dda0200f32e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 15:09:31 -0600

    Create default values

--- src/Trip.js
+++ src/Trip.js
@@ -3,11 +3,11 @@ class Trip {
     this.id = trip.id;
     this.userID = trip.userID;
     this.destinationID = trip.destinationID;
-    this.numTravelers = trip.numTravelers;
+    this.travelers = trip.travelers || 1;
     this.date = trip.date;
-    this.duration = trip.duration;
-    this.status = trip.status;
-    this.suggestedActivities = []; //will I use this??
+    this.duration = trip.duration || 1;
+    this.status = trip.status; //how do I create default status of 'pending' for new trips?
+    // this.suggestedActivities = []; //will I use this??
   }
 }
 
