e69edcf5b46e0c4a852ab61eaeef0c351bb4a717
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 14:55:53 -0600

    Add test for userID

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -1,13 +1,16 @@
 import { expect } from 'chai';
 import trips from './trips-data.js';
+import destinations from './destinations-data.js';
 import Trip from '../src/Trip.js';
 import Traveler from '../src/Trip.js'
 
 describe('Trip', () => {
-  let trip;
+  let trip, trip1;
+  let traveler;
 
   beforeEach(() => {
     trip = new Trip(trips[0])
+    trip1 = new Trip(trips[1])
   });
 
   it('should be a function', () => {
@@ -23,8 +26,18 @@ describe('Trip', () => {
       expect(trip.id).to.equal(1);
     });
 
-    it('Should have a destination id for bookings', () => {
-      expect(trip.destinationID).to.eq(49);
-    });
+    it('Should have a traveler id to associate with a traveler', () => {
+      expect(trip.userID).to.equal(44)
+    })
+
+    it('Should have a number of travelers for a booking', () => {
+      expect(trip.travelers).to.equal(1);
+      expect(trip1.travelers).to.equal(5);
+    })
+
+    //default number of travelers of 1??
+
+
+
   });
 });
