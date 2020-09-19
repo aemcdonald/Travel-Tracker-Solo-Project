7cb9cd98ba45d2e06e90df7d7be834c2f47650a6
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 16:23:18 -0600

    Create test to determine cost of a trip

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -1,46 +1,37 @@
 import { expect } from 'chai';
-import trips from './trips-data.js';
-import destinations from './destinations-data.js';
+import tripData from './trips-data.js';
+import destinationData from './destinations-data.js';
 import Trip from '../src/Trip.js';
 import Traveler from '../src/Trip.js'
 
 describe('Trip', () => {
   let trip, trip1;
   let traveler;
-
   beforeEach(() => {
-    trip = new Trip(trips[0])
-    trip1 = new Trip(trips[1])
+    trip = new Trip(tripData[0])
+    trip1 = new Trip(tripData[1])
   });
-
   it('should be a function', () => {
     expect(Trip).to.be.a('function');
   });
-
   it('should be an instance of a trip', () => {
     expect(trip).to.be.an.instanceof(Trip);
   });
-
   describe('Trip Properties', () => {
     it('should have an id', () => {
       expect(trip.id).to.equal(1);
     });
-
     it('Should have a traveler id to associate with a traveler', () => {
       expect(trip.userID).to.equal(44)
     });
-
     it('Should have a destination id', () => {
       expect(trip.destinationID).to.equal(49);
     });
-
     it('Should have a number of travelers for a booking', () => {
       expect(trip.travelers).to.equal(1);
       expect(trip1.travelers).to.equal(5);
     });
-
     //default number of travelers of 1??
-
     it('Should have a date for a booking', () => {
       expect(trip.date).to.equal('2019/09/16');
     });
@@ -57,5 +48,9 @@ describe('Trip', () => {
       expect(trip1.status).to.equal('pending');
     });
 
+    it('Should calculate the cost of a trip', () => {
+      expect(trip.calculateTripCost(destinationData)).to.equal(5819);
+      expect(trip1.calculateTripCost(destinationData)).to.equal(4565);
+    });
   });
 });
