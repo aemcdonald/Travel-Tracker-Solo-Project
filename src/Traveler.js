acd4088e124e8a7b03f3973f688aa7129b5f8b48
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-18 19:28:58 -0600

    Create getAllTrips method

--- src/Traveler.js
+++ src/Traveler.js
@@ -3,10 +3,18 @@ class Traveler {
     this.id = traveler.id;
     this.name = traveler.name;
     this.travelerType = traveler.travelerType;
+    this.allTrips = [];
     this.pastTrips = [];
     this.presentTrips = [];
     this.currentTrip = [];
     this.upcomingTrips = []
+  }
+  getAllTrips(tripsData) {
+    tripsData.forEach(trip => {
+      if (trip.userID === this.id) {
+        this.allTrips.push(trip);
+      }
+    })
   }
 }
 
