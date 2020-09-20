f78232aa5552d544ccf708ffbcbd38b2349cda3b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 20:36:22 -0600

    Remove comments, console.log

--- src/Trip.js
+++ src/Trip.js
@@ -2,11 +2,11 @@ class Trip {
   constructor(trip) {
     this.id = trip.id;
     this.userID = trip.userID;
-    this.destinationID = trip.destinationID; //49
+    this.destinationID = trip.destinationID;
     this.travelers = trip.travelers || 1;
     this.date = trip.date;
     this.duration = trip.duration || 1;
-    this.status = trip.status; //how do I create default status of 'pending' for new trips?
+    this.status = trip.status || "pending" 
     // this.suggestedActivities = []; //will I use this??
   }
   calculateTripCost(destinationData) {
