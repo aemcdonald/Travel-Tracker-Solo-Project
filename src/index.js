904ed13f04f57a901f6bb30d40a26c479c53c6f1
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-20 09:58:50 -0600

    Call domUpdates.getData, clean up variables for clarity

--- src/index.js
+++ src/index.js
@@ -11,37 +11,32 @@ import apiCalls from './apiCalls.js';
 import Traveler from './Traveler.js';
 import Destination from './Destination.js';
 import Trip from './Trip.js';
+import domUpdates from './domUpdates.js';
 
 // console.log('This is the JavaScript entry file - your code begins here.');
-let allTravelers, singleUserData, allTripData, allDestinationData;
+let destinationSelector = document.querySelector('#destination-selector');
+
+ let allUsers;
+ let singleUser;
+ let allTripData;
+ let allDestinationData;
 
 function loadData() {    //rename later?
-  let allTravelers = apiCalls.fetchAllUsersData();
-  let singleUserData = apiCalls.fetchSingleUser();
-  let allTripData = apiCalls.fetchAllTripsData();
-  let allDestinationData = apiCalls.fetchAllDestinationsData()
-  Promise.all([allTravelers, singleUserData, allTripData, allDestinationData])
-  .then(data => { //data is everything returned in promise.all
-    console.log("All Data", data)
-    allTravelers = data[0].map(traveler => {
-      return new Traveler(traveler)
-    })
-    console.log("allTravelers", allTravelers)
-    //singleUserData????
-    allTripData = data[2].map(trip => {
-      return new Trip(trip);
-    })
-    console.log("allTripData", allTripData)
-    allDestinationData = data[3].map(destination => {
-      return new Destination(destination);
-    })
-    console.log("allDestinationData", allDestinationData)
-    //let allUsers = data[0]
-    // allUsers.forEach(user => {
-    //   user = new User() //push this to an empty array at top of scripts
-    // })
-    //let allTrips = data[1] //ie whenever I'm calling my traveler this is my trips data
-    //let allDestinations = data[2]
+  let destinationsData = apiCalls.fetchAllDestinationsData()
+  Promise.all([apiCalls.fetchAllUsersData(), apiCalls.fetchSingleUser(),
+    apiCalls.fetchAllTripsData(), apiCalls.fetchAllTripsData()])
+  .then(data => {
+    allUsers = data[0].map(traveler => new Traveler(traveler))
+    singleUser = allUsers[Math.floor(Math.random() * allUsers.length)]
+    console.log("singleUserData", singleUser)
+    allTripData = data[2].map(trip => new Trip(trip))
+    allDestinationData = data[3].map(destination => new Destination(destination))
+    domUpdates.getData(allUsers, singleUser, allTripData, allDestinationData)
   })
 }
+
 loadData()
+
+function showTravelerDashboard(){
+  domUpdates.showDestinationsDropdown(allDestinationData)
+}
