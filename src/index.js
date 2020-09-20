937a6f88cb04013edac685f99679aeb2c208aaf9
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 22:37:42 -0600

    Instantiate all travelers

--- src/index.js
+++ src/index.js
@@ -6,11 +6,33 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
-import './scripts.js';
 import './index.js'; //do I need to do this?
 import apiCalls from './apiCalls.js';
 import Traveler from './Traveler.js';
 import Destination from './Destination.js';
 import Trip from './Trip.js';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
+let allTravelers, singleUserData, allTripData, allDestinationData;
+
+function loadData() {    //rename later?
+  let allTravelers = apiCalls.fetchAllUsersData();
+  let singleUserData = apiCalls.fetchSingleUser();
+  let allTripData = apiCalls.fetchAllTripsData();
+  let allDestinationData = apiCalls.fetchAllDestinationsData()
+  Promise.all([allTravelers, singleUserData, allTripData, allDestinationData])
+  .then(data => { //data is everything returned in promise.all
+    console.log("All Data", data)
+    allTravelers = data[0].map(traveler => {
+      return new Traveler(traveler)
+    })
+    //singleUserData????
+    //let allUsers = data[0]
+    // allUsers.forEach(user => {
+    //   user = new User() //push this to an empty array at top of scripts
+    // })
+    //let allTrips = data[1] //ie whenever I'm calling my traveler this is my trips data
+    //let allDestinations = data[2]
+  })
+}
+loadData()
