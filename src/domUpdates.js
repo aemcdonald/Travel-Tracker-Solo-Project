082057ac90ccbc2c63c144662dde711a53c41bfb
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-20 11:37:09 -0600

    Created showDestinationsDropdown method

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -10,7 +10,22 @@ const domUpdates = {
     this.trips = allTripData;
     this.destinations = allDestinationData;
   },
-
+  showDestinationsDropdown() {
+    let destinationSelector = document.querySelector('#destination-selector');
+    let sortedDestinations = this.destinations.sort((destination1, destination2) => {
+      if (destination1.destination < destination2.destination) {
+        return -1
+      } else {
+        return 1
+      }
+    })
+    sortedDestinations.forEach(destination => {
+      let destinationOption = `
+      <option value='${destination.destination}' id='$destination.id'>${destination.destination}</option
+      `;
+      destinationSelector.insertAdjacentHTML('beforeend', destinationOption);
+    })
+  }
 }
 
 export default domUpdates;
