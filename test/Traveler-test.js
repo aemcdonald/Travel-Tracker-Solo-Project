9410ff180993248539c73dbffa775cabdfa09fe9
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-19 18:36:22 -0600

    Change trips to tripsData for clearer variable naming

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import travelers from './travelers-data.js';
-import trips from './trips-data.js';
+import tripsData from './trips-data.js';
 import Traveler from '../src/Traveler.js';
 
 describe('Traveler', () => {
@@ -59,60 +59,66 @@ describe('Traveler', () => {
   describe('Traveler Methods', () => {
 
     it('should get all trips for a traveler', () => {
-      traveler1.getAllTrips(trips);
-      traveler2.getAllTrips(trips);
-      traveler3.getAllTrips(trips);
+      traveler1.getAllTrips(tripsData);
+      traveler2.getAllTrips(tripsData);
+      traveler3.getAllTrips(tripsData);
       expect(traveler1.allTrips.length).to.equal(1);
       expect(traveler2.allTrips.length).to.equal(6);
       expect(traveler3.allTrips.length).to.equal(7);
     });
 
     it('should get past trips for a traveler', () => {
-      traveler2.getAllTrips(trips);
+      traveler2.getAllTrips(tripsData);
       traveler2.getPastTrips("2020/09/18");
       expect(traveler2.pastTrips.length).to.equal(4);
-      traveler3.getAllTrips(trips);
+      traveler3.getAllTrips(tripsData);
       traveler3.getPastTrips("2020/09/18");
       expect(traveler3.pastTrips.length).to.equal(5);
     });
 
     it('should get the current trip for a user', () => {
-      traveler1.getAllTrips(trips);
+      traveler1.getAllTrips(tripsData);
       traveler1.getCurrentTrip("2021/01/10");
       expect(traveler1.currentTrip.length).to.equal(1);
-      traveler2.getAllTrips(trips);
+      traveler2.getAllTrips(tripsData);
       traveler2.getCurrentTrip("2020/03/06");
       expect(traveler2.currentTrip.length).to.equal(1);
     });
 
     it('should not return a trip if a user is not on a trip', () => {
-      traveler1.getAllTrips(trips);
+      traveler1.getAllTrips(tripsData);
       traveler1.getCurrentTrip("2020/09/18");
       expect(traveler1.currentTrip.length).to.equal(0);
     });
 
     it('should return a user\'s future trips', () => {
-      traveler1.getAllTrips(trips);
+      traveler1.getAllTrips(tripsData);
       traveler1.getUpcomingTrips("2020/09/18");
       expect(traveler1.upcomingTrips.length).to.equal(1);
-      traveler3.getAllTrips(trips);
+      traveler3.getAllTrips(tripsData);
       traveler3.getUpcomingTrips("2020/09/18");
       expect(traveler3.upcomingTrips.length).to.equal(1);
     });
 
     it('should not return a trip if a user is not on a trip', () => {
-      traveler1.getAllTrips(trips);
+      traveler1.getAllTrips(tripsData);
       traveler1.getCurrentTrip("2020/09/18");
       expect(traveler1.currentTrip.length).to.equal(0);
     });
 
     it('should return pending trips for a user', () => {
-      traveler1.getAllTrips(trips);
+      traveler1.getAllTrips(tripsData);
       traveler1.sortPendingTrips();
       expect(traveler1.pendingTrips.length).to.equal(0);
-      traveler2.getAllTrips(trips);
+      traveler2.getAllTrips(tripsData);
       traveler2.sortPendingTrips();
       expect(traveler2.pendingTrips.length).to.equal(1);
     });
-  })
+
+    it('Should calculate the total amount spent on trips this year', () => {
+      trip = new Trip(tripData[0])
+      traveler1.getAllTrips(tripsData); //0 trips in 2020
+      expect(traveler1.getTotalSpentThisYear()).to.equal(1);
+    });
+  });
 });
