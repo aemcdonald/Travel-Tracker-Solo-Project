0951a7b497c51749b81292663b666ff6d1c1e6df
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-09-22 08:22:04 -0600

    Add pseudocode for login page

--- src/index.js
+++ src/index.js
@@ -26,6 +26,58 @@ window.addEventListener('load', loadData)
  let tripCostButton = document.getElementById('estimated-cost-btn');
  const submitTripButton = document.querySelector('.submit-btn');
 
+ //user login
+//need event listener for username
+//need event listener for pw
+//function to login user
+  //add event listener to login button
+  //when button is clicked, fire _____
+
+//need to get userID
+  //need query selector for the userID input.value
+  //if the username includes "traveler"
+    //then assign travelerID to the input value split at r at index of 1
+    //return the traveler id
+  //else return undefined so nothing happens?????
+// domUpdates.validateLogin(){
+//
+// }
+//let loginButton = document.getElementById('login-button');
+//loginButton.addEventListener('click' attemptUserLogin)
+// function attemptUserLogin() {
+//  let username = document.getElementById('username-input')
+//  let password = document.getElementById('password-input')
+//  if (username.toLowerCase().includes('traveler'))
+
+// }
+
+
+//user login
+//need event listener for username
+//need event listener for pw
+//function to login user
+  //add event listener to login button
+  //when button is clicked, fire _____
+
+//need to get userID
+  //need query selector for the userID input.value
+  //if the username includes "traveler"
+    //then assign travelerID to the input value split at r at index of 1
+    //return the traveler id
+  //else return undefined so nothing happens?????
+// domUpdates.validateLogin(){
+//
+// }
+//let loginButton = document.getElementById('login-button');
+//loginButton.addEventListener('click' attemptUserLogin)
+// function attemptUserLogin() {
+//  let username = document.getElementById('username-input')
+//  let password = document.getElementById('password-input')
+//  if (username.toLowerCase().includes('traveler'))
+
+// }
+
+
  submitTripButton.addEventListener('click', function() {
    apiCalls.postTrip(bookTripInfo);
    loadData();
